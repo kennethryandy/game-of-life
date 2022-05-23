0aa0ef93cb21adbf259a672488114e44b5a75a08
kennethryandy <dykennethryan@gmail.com>
2022-05-24 04:34:44 +0800

    update style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import produce from 'immer';
-import { useState, useRef, useLayoutEffect, useCallback, useEffect } from 'react';
+import { useState, useRef, useLayoutEffect, useCallback } from 'react';
 
-let numRows;
-let numCols;
+let numRows = 10;
+let numCols = 10;
 
 const operations = [
 	[0, 1],
@@ -32,8 +32,8 @@ function App () {
 
 	const initializeGrid = () => {
 		const containerLayout = containerRef.current.getBoundingClientRect();
-		numCols = Math.floor(containerLayout.width / 25);
-		numRows = Math.floor(containerLayout.height / 25);
+		numCols = Math.ceil(containerLayout.width / 25);
+		numRows = Math.ceil(containerLayout.height / 25);
 		setGrid(() => {
 			let rows = [];
 			for (let i = 0; i < numRows; i++) {
@@ -59,17 +59,19 @@ function App () {
 	}
 
 	const runSimulation = useCallback(() => {
+		const nRows = numRows;
+		const nCols = numCols;
 		if (!isRunningRef.current) { return }
 
 		setGrid(currentStateGrid => {
 			return produce(currentStateGrid, currentGrid => {
-				for (let i = 0; i < numRows; i++) {
-					for (let j = 0; j < numCols; j++) {
+				for (let i = 0; i < nRows; i++) {
+					for (let j = 0; j < nCols; j++) {
 						let neighbors = 0;
 						operations.forEach(([x, y]) => {
 							const newI = i + x;
 							const newJ = j + y;
-							if ((newI >= 0 && newI < numRows) && (newJ >= 0 && newJ < numCols)) {
+							if ((newI >= 0 && newI < nRows) && (newJ >= 0 && newJ < nCols)) {
 								neighbors += currentStateGrid[newI][newJ];
 							}
 						})
@@ -83,15 +85,16 @@ function App () {
 			});
 		});
 
-		setTimeout(runSimulation, 200);
+		setTimeout(runSimulation, 100);
 	}, []);
 
-
 	return (
-		<>
+		<div className="wrapper">
+			<h1>Game Of Life</h1>
 			<div className='options'>
 				<button onClick={handleSimulationEvent}>{isRunning ? "Stop" : "Start"}</button>
 				<button onClick={() => {
+					setIsRunning(false);
 					setGrid(() => {
 						let rows = [];
 						for (let i = 0; i < numRows; i++) {
@@ -109,7 +112,7 @@ function App () {
 			<div
 				ref={containerRef}
 				className="gContainer"
-				style={{ gridTemplateColumns: `repeat(${numCols}, 25px)` }}>
+				style={{ gridTemplateColumns: `repeat(${numCols}, 25px)`, gridTemplateRows: `repeat(${numRows}, 1fr)` }}>
 				{grid.map((rows, i) => (
 					rows.map((col, j) => (
 						<div
@@ -123,10 +126,18 @@ function App () {
 							}}
 							id={`${i}_${j}`}
 							className="box" key={`${i}-${j}`}
-							style={{ backgroundColor: grid[i][j] ? "pink" : "transparent" }} />))
+							style={{ backgroundColor: grid[i][j] ? "rgb(26, 32, 39)" : "rgb(231, 235, 240)" }} />))
 				))}
 			</div>
-		</>
+			<div className="instruction">
+				<ul>
+					<li>Any live cell with fewer than two live neighbours dies, as if by underpopulation.</li>
+					<li>Any live cell with two or three live neighbours lives on to the next generation.</li>
+					<li>Any live cell with more than three live neighbours dies, as if by overpopulation.</li>
+					<li>Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.</li>
+				</ul>
+			</div>
+		</div>
 	);
 }
 
